eb6cc9fd36a6711d8deaaf5d6178667fea92a70c
Martin <martin.brusco@gmail.com>
2025-06-06 21:25:15 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Gestion de notas',
+    'description': 'Sistema de gestión de notas',
+    'category': 'base',
+    'version': '17.0.1.0',
+    'author': 'Martin Brusco',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+}
